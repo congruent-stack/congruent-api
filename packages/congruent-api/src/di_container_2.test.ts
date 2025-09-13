50314611e855397bf34ec24cdcd2e58ee9767b55
rmf <radumfilipescu@gmail.com>
2025-09-13 13:18:59 +0300

    di_scope_fixes
    - refactor #2

--- packages/congruent-api/src/di_container_2.test.ts
+++ packages/congruent-api/src/di_container_2.test.ts
@@ -2,28 +2,25 @@ import { expect, test, describe } from 'vitest';
 import { DIContainer } from './di_container_2';
 
 describe('DIContainer 2', () => {
-  test('should register and resolve services', () => {
+  test('should register and resolve transient services', () => {
     class FooService {
+      public readonly uuid = crypto.randomUUID();
     }
 
     class BarService {
-      constructor(private fooService: FooService) {}
+      public readonly uuid = crypto.randomUUID();
+      constructor(public fooService: FooService) {}
     }
 
     class BazService {
-      constructor(private barService: BarService) {}
+      public readonly uuid = crypto.randomUUID();
+      constructor(public barService: BarService) {}
     }
     
     const container = new DIContainer()
-      .register('FooService', (scope) => {
-        return new FooService();
-      }, 'transient')
-      .register('BarService', (scope) => {
-        return new BarService(scope.getFooService());
-      }, 'transient')
-      .register('BazService', (scope) => {
-        return new BazService(scope.getBarService());
-      }, 'transient');
+      .register('FooService', () => new FooService(), 'transient')
+      .register('BarService', (scope) => new BarService(scope.getFooService()), 'transient')
+      .register('BazService', (scope) => new BazService(scope.getBarService()), 'transient');
 
     const scope = container.createScope();
 
@@ -34,5 +31,82 @@ describe('DIContainer 2', () => {
     expect(fooService).toBeInstanceOf(FooService);
     expect(barService).toBeInstanceOf(BarService);
     expect(bazService).toBeInstanceOf(BazService);
+
+    expect(fooService.uuid).not.toBe(barService.fooService.uuid);
+    expect(fooService.uuid).not.toBe(bazService.barService.fooService.uuid);
+
+    expect(barService.uuid).not.toBe(bazService.barService.uuid);
+  });
+
+  test('should register and resolve scoped services', () => {
+    class FooService {
+      public readonly uuid = crypto.randomUUID();
+    }
+
+    class BarService {
+      public readonly uuid = crypto.randomUUID();
+      constructor(public fooService: FooService) {}
+    }
+
+    class BazService {
+      public readonly uuid = crypto.randomUUID();
+      constructor(public barService: BarService) {}
+    }
+    
+    const container = new DIContainer()
+      .register('FooService', () => new FooService(), 'scoped')
+      .register('BarService', (scope) => new BarService(scope.getFooService()), 'scoped')
+      .register('BazService', (scope) => new BazService(scope.getBarService()), 'scoped');
+
+    const scope = container.createScope();
+
+    const bazService = scope.getBazService();
+    const barService = scope.getBarService();
+    const fooService = scope.getFooService();
+
+    expect(fooService).toBeInstanceOf(FooService);
+    expect(barService).toBeInstanceOf(BarService);
+    expect(bazService).toBeInstanceOf(BazService);
+
+    expect(fooService.uuid).toBe(barService.fooService.uuid);
+    expect(fooService.uuid).toBe(bazService.barService.fooService.uuid);
+
+    expect(barService.uuid).toBe(bazService.barService.uuid);
+  });
+
+  test('should register and resolve singleton services', () => {
+    class FooService {
+      public readonly uuid = crypto.randomUUID();
+    }
+
+    class BarService {
+      public readonly uuid = crypto.randomUUID();
+      constructor(public fooService: FooService) {}
+    }
+
+    class BazService {
+      public readonly uuid = crypto.randomUUID();
+      constructor(public barService: BarService) {}
+    }
+    
+    const container = new DIContainer()
+      .register('FooService', () => new FooService(), 'singleton')
+      .register('BarService', (scope) => new BarService(scope.getFooService()), 'singleton')
+      .register('BazService', (scope) => new BazService(scope.getBarService()), 'singleton');
+
+    const scope = container.createScope();
+
+    const bazService = scope.getBazService();
+    const barService = scope.getBarService();
+    const fooService = scope.getFooService();
+
+    expect(fooService).toBeInstanceOf(FooService);
+    expect(barService).toBeInstanceOf(BarService);
+    expect(bazService).toBeInstanceOf(BazService);
+    
+    expect(fooService.uuid).toBe(barService.fooService.uuid);
+    expect(fooService.uuid).toBe(bazService.barService.fooService.uuid);
+
+    expect(barService.uuid).toBe(bazService.barService.uuid);
   });
 });
--- packages/congruent-api/src/di_container_2.ts
+++ packages/congruent-api/src/di_container_2.ts
@@ -16,6 +16,7 @@ export type DIScope<R extends DIRegistry> = {
 
 export class DIContainer<R extends DIRegistry = {}> {
   private _map = new Map<string, DIRegistryEntry<any>>();
+  private _singletonInstances = new Map<string, any>();
 
   public register<K extends string, T>(
     serviceNameLiteral: StringLiteral<K>,
@@ -28,16 +29,37 @@ export class DIContainer<R extends DIRegistry = {}> {
   }
 
   public createScope(): DIScope<R> {
-    const proxy = new Proxy({}, {
-      get: (_target, prop: string) => {
+    const proxy = new Proxy({
+      _map: this._map,
+      _singletonInstances: this._singletonInstances,
+      _scopedInstances: new Map<string, any>(),
+    }, {
+      get: (target, prop: string) => {
         if (prop.startsWith('get')) {
           const serviceName = prop.slice(3);
-          if (this._map.has(serviceName)) {
-            return () => {
-              const entry = this._map.get(serviceName);
-              if (!entry) throw new Error(`Service not found: ${serviceName}`);
-              return entry.factory(proxy);
-            };
+          if (target._map.has(serviceName)) {
+            const entry = target._map.get(serviceName)!;
+            switch (entry.lifetime) {
+              case 'transient':
+                return () => entry.factory(proxy);
+              case 'scoped':
+                return () => {
+                  if (!target._scopedInstances.has(serviceName)) {
+                    const instance = entry.factory(proxy);
+                    target._scopedInstances.set(serviceName, instance);
+                  }
+                  return target._scopedInstances.get(serviceName);
+                };
+              case 'singleton':
+                return () => {
+                  if (!target._singletonInstances.has(serviceName)) {
+                    const instance = entry.factory(proxy);
+                    target._singletonInstances.set(serviceName, instance);
+                  }
+                  return target._singletonInstances.get(serviceName);
+                };
+            }
+            // throw new Error(`Unsupported lifetime: ${entry.lifetime}`);
           } else {
             throw new Error(`Service not registered: ${serviceName}`);
           }
@@ -45,6 +67,6 @@ export class DIContainer<R extends DIRegistry = {}> {
         throw new Error(`Service not found: ${String(prop)}`);
       }
     });
-    return proxy as DIScope<R>;
+    return proxy as unknown as DIScope<R>;
   }
 }
